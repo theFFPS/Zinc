71d54a194e8ef6d7e27f4d3788e6ab435fdfecce
theFfps <shmelev.lev@gmail.com>
2025-04-20 12:00:29 +0300

    Add conan

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p build
+conan install . --output-folder=build --build=missing
+cmake --preset conan-release
+cmake --build build
---
+++ clear.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf build .build CMakeUserPresets.json conanbuild.sh conanrun.sh
